07cb587752d5ed6864629f63f901cc5cde142aa1
Pranav Vinayak Jadhav <pj8841466@gmail.com>
2022-09-20 11:58:22 +0530

    lol

---
+++ prac6.py
@@ -0,0 +1,25 @@
+#Implement decision tree learning algorithm for the restaurant waiting problem.
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+import pandas as pd
+
+dataset = pd.read_csv('restaurants.csv')
+
+
+train_features = dataset.iloc[:80,:-1]
+
+test_features = dataset.iloc[80:,:-1]
+
+
+train_targets = dataset.iloc[:80,-1]
+test_targets = dataset.iloc[80:,-1]
+
+
+
+tree = DecisionTreeClassifier(criterion = 'entropy').fit(train_features,train_targets)
+
+
+prediction = tree.predict(test_features)
+
+
+print("The prediction accuracy is: ",tree.score(test_features,test_targets)*100,"%")
---
+++ prac7.py
@@ -0,0 +1,26 @@
+#Implement Naïve Bayes learning algorithm for the restaurant waiting problem. rendering
+# Gaussian Naive Bayes
+from sklearn import datasets
+from sklearn import metrics
+from sklearn.naive_bayes import GaussianNB
+# load the iris datasets
+#Import the dataset
+import pandas as pd 
+dataset = pd.read_csv('restaurants.csv')
+
+#dataset = datasets.load_iris()
+# fit a Naive Bayes model to the data
+model = GaussianNB()
+model.fit(dataset.iloc[:90,0:8],dataset.iloc[:90,-1])
+#model.fit(dataset.data, dataset.target)
+print(model)
+# make predictions
+#expected = dataset.target
+#predicted = model.predict(dataset.data)
+
+expected = dataset.iloc[:90,-1]
+predicted = model.predict(dataset.iloc[:90,0:8])
+# summarize the fit of the model
+print(metrics.classification_report(expected, predicted))
+print(metrics.confusion_matrix(expected, predicted))
+
